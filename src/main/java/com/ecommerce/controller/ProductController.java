498c0f96da8d3cb83de1af396d8f244bd7e66710
Agustin <aaragon@alumnos.exa.unicen.edu.ar>
2024-01-15 09:52:46 -0300

    funcionalidad de eliminar productos

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -3,12 +3,14 @@ package com.ecommerce.controller;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +62,43 @@ public class ProductController {
 		User u = new User(1, "", "", "", "", "", "", "");
 		product.setUser(u);
 		productService.save(product);
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id,Model model) {
+		Product product = new Product();
+		Optional<Product> optionalProduct = productService.get(id);
+		product = optionalProduct.get();
+		
+		LOGGER.info("producto buscado: {}",product);
+		model.addAttribute("product", product);
+		return "products/edit";
+		
+	}
+	
+	@PostMapping("/update")
+	public String update(Product product,@RequestParam("file") MultipartFile imgform) {
+		if (!imgform.isEmpty()) {
+			Path imgDirectory = Paths.get("src//main//resources//static/images");
+			String absolutePath =imgDirectory.toFile().getAbsolutePath();
+			try {
+				byte[] bytesImg = imgform.getBytes();
+				Path fullPath = Paths.get(absolutePath + "//" + imgform.getOriginalFilename());
+				Files.write(fullPath, bytesImg);
+				product.setImg(imgform.getOriginalFilename());
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		productService.update(product);
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productService.delete(id);
 		return "redirect:/products";
 	}
 	
