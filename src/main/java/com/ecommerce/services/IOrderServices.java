e8647d9d714ca67f22f62fad8659f1c599986f24
Agustin <aaragon@alumnos.exa.unicen.edu.ar>
2024-01-20 20:51:02 -0300

    generador de numero de orden

--- src/main/java/com/ecommerce/services/IOrderServices.java
+++ src/main/java/com/ecommerce/services/IOrderServices.java
@@ -1,8 +1,11 @@
 package com.ecommerce.services;
 
+import java.util.List;
+
 import com.ecommerce.models.Order;
 
 public interface IOrderServices {
 
+	List<Order> findAll();
 	Order Save(Order order);
 }
--- src/main/java/com/ecommerce/services/OrderServicesImpl.java
+++ src/main/java/com/ecommerce/services/OrderServicesImpl.java
@@ -1,5 +1,8 @@
 package com.ecommerce.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +21,35 @@ public class OrderServicesImpl implements IOrderServices{
 		return orderRepository.save(order);
 	}
 
+	@Override
+	public List<Order> findAll() {
+		// TODO Auto-generated method stub
+		return orderRepository.findAll();
+	}
+
+	public String generateOrderNumber() {
+		int num=0;
+		String concatNum="";
+		
+		List<Order> orders = findAll();
+		
+		List<Integer> numbers = new ArrayList<Integer>();
+		
+		orders.stream().forEach(o-> numbers.add(Integer.parseInt(o.getNumber())));
+		
+		if(orders.isEmpty()) {
+			num = 1;
+		}else {
+			num = numbers.stream().max(Integer::compare).get();	
+			num++;
+		}
+		
+		concatNum = String.valueOf(num);
+		int digits = concatNum.length();
+		
+		for (int j=digits;j<=9;j++) {
+			concatNum="0"+concatNum;
+		}
+		return concatNum;
+	}
 }
